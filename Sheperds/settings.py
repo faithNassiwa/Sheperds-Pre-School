6dcc9f970774f75817159a001052f51708524972
faithNassiwa <faithnassiwa@gmail.com>
2016-10-24 12:23:23 +0300

    updated settings

--- Sheperds/settings.py
+++ Sheperds/settings.py
@@ -183,7 +183,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [''sheperdspreschool.herokuapp.com', 'localhost'']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
