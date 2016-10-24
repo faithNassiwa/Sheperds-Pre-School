816fca897cca54845f748bf1ad61ee6e2ade5dcb
faithNassiwa <faithnassiwa@gmail.com>
2016-10-24 15:54:06 +0300

    updated settings

--- Sheperds/settings.py
+++ Sheperds/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '-)=vzzr9hc6_4^v(5w9_s^=5c3bs%(3@v5dlx9v)ixq4#q)*3l'
 ALLOWED_HOSTS = ['sheperdspreschool.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 
